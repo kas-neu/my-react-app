f105910dca4d8adb02ab7f130285faf31e399574
kas-neu <153776294+kas-neu@users.noreply.github.com>
2024-04-29 13:11:11 +0200

    created search bar and edited h1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Weather from "./Weather";
 
@@ -6,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world</h1>
+        <h1>Weather App</h1>
         <Weather />
       </header>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,18 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-  return <h2>Hello from Weather</h2>;
+  return (
+    <div className="bar">
+      <form className="search-form" id="search-form">
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          required
+          className="search-form-input"
+        />
+        <input type="submit" value="Search" className="search-form-button" />
+      </form>
+    </div>
+  );
 }
