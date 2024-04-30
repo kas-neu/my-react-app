3fb94716e0ad466b7f340dfc7fe22a33e36e8138
kas-neu <153776294+kas-neu@users.noreply.github.com>
2024-04-30 17:33:23 +0200

    added footer section

--- src/App.js
+++ src/App.js
@@ -9,6 +9,26 @@ function App() {
         <h1>Weather App</h1>
         <Weather />
       </header>
+      <div>
+        {" "}
+        <footer>
+          <p>
+            This app was coded by Kasia, is open-sourced on{" "}
+            <a href="https://github.com/kas-neu/my-react-app" target="_blank">
+              Github
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://kas-weatherapp-react.netlify.app/?"
+              target="_blank"
+            >
+              {" "}
+              Netlify
+            </a>
+            .
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
